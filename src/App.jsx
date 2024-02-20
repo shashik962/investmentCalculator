c25c70c46cc18dfb10ac0782dea7a5cb63a34e19
shashi kumar <shashikumar@shashis-MacBook-Air.local>
2024-02-20 22:49:56 +0530

    Computing Values & Properly Handling Number

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
     setUserInput(prevUserInput => {
           return {
                 ...prevUserInput,
-                [inputIdentifier]: newValue
+                [inputIdentifier]: +newValue
           }
     });
   }
