3ec5368c9d77659c8c54c2a6fee03ed813ef7a4e
shashi kumar <shashikumar@shashis-MacBook-Air.local>
2024-02-20 21:37:32 +0530

    userInputOrTwoWayBinding

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header.jsx"
+import UserInput from "./components/UserInput.jsx"
+
+function App() {
+  return (
+    <>
+       <Header />
+       <UserInput />
+    </>
+   
+  )
+}
+
+export default App
