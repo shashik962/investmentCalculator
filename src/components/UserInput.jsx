b1334e0be4455a3087d9b786a31e4759bef7c10e
shashi kumar <shashikumar@shashis-MacBook-Air.local>
2024-02-20 22:11:10 +0530

    Lifting State Up

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,24 +1,5 @@
 
-import { useState } from 'react';
-
-export default function UserInput() {
-
-      const [userInput, setUserInput] = useState({
-            initialInvestment: 1000,
-            annualInvestment: 1200,
-            expectedReturn: 6,
-            duration: 10
-      });
-
-      function handleChange(inputIdentifier, newValue) {
-            setUserInput(prevUserInput => {
-                  return {
-                        ...prevUserInput,
-                        [inputIdentifier]: newValue
-                  }
-            });
-      }
-
+export default function UserInput({onChange, userInput}) {
       return (
            <section id="user-input">
                   <div className="input-group">
@@ -29,7 +10,7 @@ export default function UserInput() {
                                     required 
                                     value={userInput.initialInvestment} 
                                     onChange={(event) => 
-                                          handleChange('initialInvestment', event.target.value) 
+                                          onChange('initialInvestment', event.target.value) 
                                     } 
                               />
                         </p>
@@ -40,7 +21,7 @@ export default function UserInput() {
                                     required 
                                     value={userInput.annualInvestment} 
                                     onChange={(event) => 
-                                          handleChange('annualInvestment', event.target.value) 
+                                          onChange('annualInvestment', event.target.value) 
                                     } 
                               />
                         </p>
@@ -53,7 +34,7 @@ export default function UserInput() {
                                     required 
                                     value={userInput.expectedReturn} 
                                     onChange={(event) => 
-                                          handleChange('expectedReturn', event.target.value) 
+                                          onChange('expectedReturn', event.target.value) 
                                     } 
                               />
                         </p>
@@ -64,7 +45,7 @@ export default function UserInput() {
                                     required 
                                     value={userInput.duration} 
                                     onChange={(event) => 
-                                          handleChange('duration', event.target.value) 
+                                          onChange('duration', event.target.value) 
                                     } 
                               />
                         </p>
